50bc66af39acb81d17c087cd70c8c67fff05931b
Nicolas <playchathac@gmail.com>
2026-07-01 18:39:40 +0200

    Fix: create folder pub

--- src/save.rs
+++ src/save.rs
@@ -1,5 +1,5 @@
 use dialoguer::Input;
-use std::fs::File;
+use std::fs;
 use std::io::{self, Write, Read};
 
 struct State {
@@ -28,7 +28,11 @@ pub fn save(history: &mut Vec<String>) {
 
     let route: String = format!("pub/{input}.json");
 
-    let mut file = File::create(&route).expect("Failed to create file (check names)");
+    if let Err(e) = fs::create_dir_all("pub") {
+    panic!("No se pudo crear la carpeta 'pub': {}", e);
+}
+
+    let mut file = fs::File::create(&route).expect("Failed to create file (check names)");
     file.write_all(text.as_bytes()).expect("Failed to write to file");
 
     println!("Saving state...");
