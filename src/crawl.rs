4ba3eb6a26273ce3247b700b62a8a84db938fa66
Nicolas <playchathac@gmail.com>
2026-07-04 18:58:46 +0200

    Getting robots url

--- src/crawl.rs
+++ src/crawl.rs
@@ -1,4 +1,5 @@
 use dialoguer::Input;
+use url::Url;
 
 pub fn crawl(history: &mut Vec<String>) {
     let input: String = Input::new()
@@ -8,5 +9,27 @@ pub fn crawl(history: &mut Vec<String>) {
         .expect("Failed to read line");
     
     history.push(input.clone());
-    println!("Crawling URL: {input}");
+
+    let mut domain = get_robot(&input).expect("Failed to get robots.txt URL");
+    println!("Crawling URL: {domain}");
+}
+
+
+fn get_robot(url: &str) -> Result<String, url::ParseError> {
+    let parsed_url = url::Url::parse(url)?;
+    // http or https
+    let scheme = parsed_url.scheme();
+    
+    let domain = match parsed_url.host_str() {
+        Some(domain) => domain,
+        None => return Err(url::ParseError::EmptyHost),
+    };
+
+    let robots = if let Some(port) = parsed_url.port() {
+        format!("{}://{}:{}/robots.txt", scheme, domain, port)
+    } else {
+        format!("{}://{}/robots.txt", scheme, domain)
+    };
+    
+    Ok(robots)
 }
