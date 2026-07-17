0c190acbc0c335561258ee9533614657d9e0e203
Nicolas <playchathac@gmail.com>
2026-07-17 11:11:14 +0200

    Read db manage func

--- src/manage.rs
+++ src/manage.rs
@@ -37,4 +37,40 @@ pub fn manage(history: &mut Vec<String>, conn: &rusqlite::Connection) {
         .default(0)
         .interact()
         .expect("Failed to read selection");
+
+    if selection == 0 {
+        history.push("Add User".to_string());
+
+    } else if selection == 1 {
+        history.push("Delete User".to_string());
+    } else if selection == 2 {
+        history.push("List Users".to_string());
+
+    } else if selection == 3 {
+        history.push("Read db".to_string());
+        let mut urls = conn.prepare("SELECT * FROM urls").expect("Failed to prepare statement");
+        let url_iter = urls.query_map([], |row| {
+            let id: i32 = row.get(0)?;
+            let url: String = row.get(1)?;
+            Ok((id, url))
+        }).expect("Failed to query urls");
+
+        for url in url_iter {
+            match url {
+                Ok((id, url)) => {
+                    println!("ID: {}, URL: {}", id, url);
+                }
+                Err(err) => {
+                    // println!("Error reading URL: {}", err);
+                }
+            }
+        }
+
+    } else if selection == 4 {
+        history.push("Exit".to_string());
+
+    } else {
+        history.push("Invalid selection".to_string());
+        panic!("Invalid selection");
+    }
 }
