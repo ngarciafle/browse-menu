e24cd3563bcec6af6cf343e7b4503a6d1ae0247d
Nicolas <playchathac@gmail.com>
2026-07-30 10:46:00 +0200

    Delete user impl

--- src/remove_user.rs
+++ src/remove_user.rs
@@ -6,9 +6,43 @@ use rusqlite::Connection;
 // Maybe import log in ??
 
 pub fn remove_user(history: &mut Vec<String>, conn: &Connection) {
+    // Must change expect to match to handle errors
+
+
     // First check if admin is logged in & log in case
+    // Implementing showing users & then asking for the password
 
     // Then show usernames
+    let mut stmt = conn.prepare("SELECT username FROM credentials").expect("Failed to prepare statement");
+    let usernames_iter = stmt.query_map([], |row| row.get(0)).expect("Failed to query usernames");
+    let usernames: Vec<String> = usernames_iter.map(|res| res.expect("Failed to get username")).collect();
+
+    let username = Select::new()
+        .with_prompt("Select a user to remove")
+        .items(&usernames)
+        .default(0)
+        .interact()
+        .expect("Failed to read selection");
+    let selected_username = &usernames[username];
+
+    // Check the password
+    let password: String = Password::new()
+        .with_prompt("Enter your password")
+        .interact()
+        .expect("Failed to read line");
+
+    let result_cred = conn.query_row(
+        "SELECT password FROM credentials WHERE username = ?1",
+        &[selected_username],
+        |row| row.get(0),
+    );
+    let hashed_password = result_cred.expect("Failed to get hashed password");
+
+    if !bcrypt::verify(password, &hashed_password).unwrap_or(false) {
+        println!("Incorrect password.");
+        history.push(format!("Failed to remove user: {} - incorrect password", selected_username));
+        return;
+    }
 
     // Delete the user from the database
     match conn.execute(
@@ -29,4 +63,5 @@ pub fn remove_user(history: &mut Vec<String>, conn: &Connection) {
             history.push(format!("Failed to remove user: {} - {}", username, e));
         }
     }
+    //Check if there are no users left in the database, if so, create a new admin user
 }
